ae344bda74a747f515a8904ef60a18a247346ee9
Gary Lo <logap@me.com>
2021-06-23 08:10:50 +0800

    add ping script

---
+++ ping.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const { Pool } = require('pg')
+const logger = require('pino')()
+
+async function ping() {
+  const pool = new Pool({
+    connectionString: process.env.PGCONNECTIONSTRING,
+    max: process.env.PGMAXCONN,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 30000
+  })
+  await pool.connect()
+
+  const { rows } = await pool.query('SELECT NOW()')
+
+  logger.info({ rows, 'process.env.PGMAXCONN': process.env.PGMAXCONN })
+  // release pool before exist
+  pool.end()
+
+  process.exit(0)
+}
+
+ping()
