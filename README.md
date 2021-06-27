4bf2e0eb64d8a2bd90c81bcdb8616e0b12b353ca
Gary Lo <logap@me.com>
2021-06-28 04:55:14 +0800

    switch to time-based approach

--- README.md
+++ README.md
@@ -68,16 +68,16 @@ yarn insert --worker=8 --concurrency=8000 --maxDbConnection=500 --numOfDataSet=2
 # query={0,1,2,3} light workload weight query(all hit index with random parameters)
 # query={4} medium workload query (all hit index with random parameters but large amount of data join)
 # query={5} heavy workload query (table scan and large amount of data join)
-yarn query --query=0  --concurrency=2000 --maxDbConnection=250 --numOfQuerySet=100000 --randomSeed=10 > output/q0.txt \
+yarn query --query=0  --concurrency=2000 --maxDbConnection=250 --period=180 --randomSeed=10 > output/q0.txt \
 && sleep 1m \
-&& yarn query --query=1  --concurrency=1000 --maxDbConnection=250 --numOfQuerySet=20000 --randomSeed=10 > output/q1.txt \
+&& yarn query --query=1  --concurrency=1000 --maxDbConnection=250 --period=180 --randomSeed=10 > output/q1.txt \
 && sleep 1m \
-&& yarn query --query=2  --concurrency=1000 --maxDbConnection=250 --numOfQuerySet=20000 --randomSeed=10 > output/q2.txt \
+&& yarn query --query=2  --concurrency=1000 --maxDbConnection=250 --period=180 --randomSeed=10 > output/q2.txt \
 && sleep 1m \
-&& yarn query --query=3  --concurrency=1000 --maxDbConnection=250 --numOfQuerySet=4000 --randomSeed=10 > output/q3.txt \
+&& yarn query --query=3  --concurrency=500 --maxDbConnection=250 --period=180 --randomSeed=10 > output/q3.txt \
 && sleep 1m \
-&& yarn query --query=4  --concurrency=100 --maxDbConnection=250 --numOfQuerySet=400 --randomSeed=10 > output/q4.txt \
+&& yarn query --query=4  --concurrency=100 --maxDbConnection=250 --period=180 --randomSeed=10 > output/q4.txt \
 && sleep 1m \
-&& yarn query --query=5  --concurrency=100 --maxDbConnection=250 --numOfQuerySet=400 --randomSeed=10 > output/q5.txt
+&& yarn query --query=5  --concurrency=100 --maxDbConnection=250 --period=180 --randomSeed=10 > output/q5.txt
 
 ```
--- src/query.js
+++ src/query.js
@@ -1,24 +1,22 @@
+const { timeElapsedInSecondsSince } = require('./utils/utils')
 const { runBenchmark } = require('./run-benchmark')
 require('dotenv').config()
 const logger = require('pino')()
 const { argv } = require('yargs/yargs')(process.argv.slice(2))
-const { concurrency = 2000, maxDbConnection = 50, numOfQuerySet = 5000, query = 0 } = argv
+const { concurrency = 2000, maxDbConnection = 50, period = 60, query = 0 } = argv
 const { ReadQueries } = require('./sql/read-queries')
 
+const randomCount = 100_000
 let queried = 0
 let error = 0
+let finished = false
 
 async function busyDispatcher(pool, jobs) {
   let cursor = 0
-  while (jobs.length > 0) {
+  while (!finished && jobs.length > 0) {
     const index = cursor++ % jobs.length
     const [query, paramList] = jobs[index]
-    if (paramList.length === 0) {
-      // remove the job from job list
-      jobs.splice(index, 1)
-      continue
-    }
-    const param = paramList.pop()
+    const param = paramList[cursor % paramList.length]
     try {
       await pool.query(query, param)
       queried++
@@ -47,24 +45,12 @@ async function busyDispatcher(pool, jobs) {
 
       const jobMap = new Map([
         // support multiple jobs structures
-        [0, () => [[ReadQueries.query0SQL, ReadQueries.query0Params(numOfQuerySet)]]],
-        [
-          1,
-          () => [[ReadQueries.query1SQL, ReadQueries.query1Params(numOfQuerySet, Number(count))]]
-        ],
-        [
-          2,
-          () => [[ReadQueries.query2SQL, ReadQueries.query2Params(numOfQuerySet, Number(count))]]
-        ],
-        [
-          3,
-          () => [[ReadQueries.query3SQL, ReadQueries.query3Params(numOfQuerySet, Number(count))]]
-        ],
-        [
-          4,
-          () => [[ReadQueries.query4SQL, ReadQueries.query4Params(numOfQuerySet, Number(count))]]
-        ],
-        [5, () => [[ReadQueries.query5SQL, ReadQueries.query5Params(numOfQuerySet, Number(count))]]]
+        [0, () => [[ReadQueries.query0SQL, ReadQueries.query0Params(randomCount)]]],
+        [1, () => [[ReadQueries.query1SQL, ReadQueries.query1Params(randomCount, Number(count))]]],
+        [2, () => [[ReadQueries.query2SQL, ReadQueries.query2Params(randomCount, Number(count))]]],
+        [3, () => [[ReadQueries.query3SQL, ReadQueries.query3Params(randomCount, Number(count))]]],
+        [4, () => [[ReadQueries.query4SQL, ReadQueries.query4Params(randomCount, Number(count))]]],
+        [5, () => [[ReadQueries.query5SQL, ReadQueries.query5Params(randomCount, Number(count))]]]
       ])
 
       // create target job
@@ -80,18 +66,23 @@ async function busyDispatcher(pool, jobs) {
         queryRate: 0,
         timeElapsedInSeconds: 0
       })
-      const getTimeElapsedInSeconds = () => Number((new Date().getTime() - start) / 1000).toFixed(2)
-      const getProgress = () => Number(queried / totalQueryCount).toFixed(4)
-      const getRate = () => Number(queried / getTimeElapsedInSeconds()).toFixed(2)
+      const getProgress = () =>
+        Math.min(1, Number(timeElapsedInSecondsSince(start) / period)).toFixed(4)
+      const getRate = () => Number(queried / timeElapsedInSecondsSince(start)).toFixed(2)
       const displayProgressInterval = setInterval(() => {
         logger.info({
           queried,
           progress: getProgress(),
           queryRate: `${getRate()}/s`,
           error,
-          timeElapsedInSeconds: getTimeElapsedInSeconds()
+          timeElapsedInSeconds: timeElapsedInSecondsSince(start)
         })
       }, 1000)
+
+      // benchmark for a specific period
+      setTimeout(() => {
+        finished = true
+      }, period * 1000)
 
       const queryPs = new Array(Math.max(concurrency, 1))
         .fill(null)
@@ -106,7 +97,7 @@ async function busyDispatcher(pool, jobs) {
         progress: getProgress(),
         queryRate: `${getRate()}/s`,
         error,
-        timeElapsedInSeconds: getTimeElapsedInSeconds()
+        timeElapsedInSeconds: timeElapsedInSecondsSince(start)
       })
     }
   )
